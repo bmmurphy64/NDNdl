70ec06a2e1dd20bf6129914b3880f50323fbcd87
Ben Murphy <bmmurphy@memphis.edu>
2015-08-21 14:14:18 -0500

    Updated logging to store file in a better location. Added CPU log parsing result to main log file. Cleaned up code in preparation for review. Slight restructuring of log file.

--- app/src/main/java/com/ndn/bmmurphy/ndndl/MainActivity.java
+++ app/src/main/java/com/ndn/bmmurphy/ndndl/MainActivity.java
@@ -12,6 +12,7 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Spinner;
+import android.widget.TextView;
 
 import net.named_data.jndn.Data;
 import net.named_data.jndn.Face;
@@ -20,210 +21,228 @@ import net.named_data.jndn.Name;
 import net.named_data.jndn.OnData;
 import net.named_data.jndn.OnTimeout;
 
-import java.io.BufferedWriter;
-import java.io.File;
-import java.io.FileWriter;
 import java.util.HashMap;
 
 
 public class MainActivity extends ActionBarActivity {
 
-    private final String TAG = "NDNdl";
+  private final String TAG = "NDNdl";
+  private final String TIMEOUT_MSG = "Error: All Interests timed out. " +
+      "Check your NFD configuration and make sure " +
+      "the location you're downloading from is up.";
+  private final String SUCCESS_MSG = "Test completed successfully." +
+      "Results are stored in Documents/NDNdl_results.log";
 
-    private Spinner _sizeSelect;
-    private Spinner _locationSelect;
-    private PowerManager _mgr;
-    private PowerManager.WakeLock wakelock;
-    private ProgressDialog _proDlg;
-    private EditText _nIterations;
-    private EditText _timeOut;
-    private Button _testButton;
 
-    private final HashMap<String, String> _prefixes = new HashMap<>();
+  private Spinner locationSelect_;
+  private PowerManager.WakeLock wakelock_;
+  private ProgressDialog proDlg_;
+  private EditText nIterations_;
+  private EditText timeOut_;
+  private TextView errorPanel_;
+
+  private HashMap<String, String> prefixes_ = new HashMap<>();
+
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.activity_main);
+    locationSelect_ = (Spinner) findViewById(R.id.locations);
+    nIterations_ = (EditText) findViewById(R.id.numIts);
+    timeOut_ = (EditText) findViewById(R.id.timeout);
+    errorPanel_ = (TextView) findViewById(R.id.errorView);
+    Button testStartButton_ = (Button) findViewById(R.id.beginTest);
+    testStartButton_.setOnClickListener(btnClickListener);
+    prefixes_.put("Memphis", "/ndn/edu/memphis/download/");
+    prefixes_.put("St. Louis", "/ndn/edu/wustl/memphis/download/");
+    prefixes_.put("Switzerland", "/ndn/ch/unibas/memphis/download/");
+    prefixes_.put("Korea", "/ndn/kr/anyang/memphis/download/");
+    prefixes_.put("Los Angeles", "/ndn/org/caida/download/");
+  }
+
+  @Override
+  public boolean onCreateOptionsMenu(Menu menu) {
+    // Inflate the menu; this adds items to the action bar if it is present.
+    getMenuInflater().inflate(R.menu.menu_main, menu);
+    return true;
+  }
+
+  @Override
+  public boolean onOptionsItemSelected(MenuItem item) {
+    // Handle action bar item clicks here. The action bar will
+    // automatically handle clicks on the Home/Up button, so long
+    // as you specify a parent activity in AndroidManifest.xml.
+    int id = item.getItemId();
+
+    //noinspection SimplifiableIfStatement
+    if (id == R.id.action_settings) {
+      return true;
+    }
+
+    return super.onOptionsItemSelected(item);
+  }
+
+  View.OnClickListener btnClickListener = new View.OnClickListener() {
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        _sizeSelect = (Spinner) findViewById(R.id.sizeSelect);
-        _locationSelect = (Spinner) findViewById(R.id.locations);
-        _nIterations = (EditText) findViewById(R.id.numIts);
-        _timeOut = (EditText) findViewById(R.id.timeout);
-        _testButton = (Button) findViewById(R.id.beginTest);
-        _testButton.setOnClickListener(btnClickListener);
-        _prefixes.put("Memphis", "/ndn/edu/memphis/download/");
-        _prefixes.put("St. Louis", "/ndn/edu/wustl/memphis/download/");
-        _prefixes.put("Switzerland", "/ndn/ch/unibas/memphis/download/");
-        _prefixes.put("Korea", "/ndn/kr/anyang/memphis/download/");
-        _prefixes.put("Los Angeles", "/ndn/org/caida/download/");
+    public void onClick(View v) {
+
+      // TODO Auto-generated method stub
+      DownloadThread thread;
+
+      proDlg_ = ProgressDialog.show(MainActivity.this, "", "waiting...");
+
+      PowerManager mgr_ = (PowerManager)
+          getApplicationContext().getSystemService(
+              Context.POWER_SERVICE);
+      wakelock_ = mgr_.newWakeLock(PowerManager.PARTIAL_WAKE_LOCK,
+          "MyWakeLock");
+      wakelock_.acquire();
+
+      int dlSize = Integer.parseInt(nIterations_.getText().toString()) *
+          1024;
+
+      //Total number of segments to download is total size/8
+      //as traffic generator's max size is 8KB
+
+      dlSize = dlSize/8;
+      int timeOut = Integer.parseInt(timeOut_.getText().toString());
+
+      String downloadLocation =
+          locationSelect_.getSelectedItem().toString();
+
+      thread = new DownloadThread(dlSize, timeOut, downloadLocation);
+
+      thread.start();
+    }
+  };
+
+  private class PingTimer implements OnData, OnTimeout
+  {
+    private long startTime;
+    private long elapsedTime = 0;
+    private Statistics statistics;
+
+    public PingTimer(Statistics stats) {
+      statistics = stats;
+    }
+
+    public void onData(Interest interest, Data data) {
+      elapsedTime = System.currentTimeMillis() - this.startTime;
+
+      statistics.addItem(elapsedTime);
+
+      String name = data.getName().toUri();
+      String pingTarget = name.substring(0, name.lastIndexOf("/"));
+      String contentStr = pingTarget + ": " +
+              String.valueOf(elapsedTime) + " ms";
+      Log.i(TAG, "Content " + contentStr);
+
+    }
+
+    public void onTimeout(Interest interest) {
+      statistics.addTimeout();
+      Log.i(TAG, "Time out for interest " + interest.getName().toUri());
+    }
+
+    public void startUp() {
+      startTime = System.currentTimeMillis();
+    }
+  }
+
+  private class DownloadThread extends Thread {
+    private int nIterations_;
+    private int timeOut_;
+    private String downloadPrefix_;
+    private Statistics statistics_;
+
+    public DownloadThread(int iterations, int timeout, String dlLoc) {
+	  if (iterations > 0) {
+		nIterations_ = iterations;  
+	  }
+	  else {
+		nIterations = 100;  
+	  }
+	  if (timeout > 0) {
+		timeOut_ = timeout;  
+	  }
+	  else {
+		  timeOut_ = 1000;
+	  }
+      downloadPrefix_ = prefixes_.get(dlLoc);
+      statistics_ = new Statistics(
+          MainActivity.this.getApplicationContext(),
+          timeOut_, dlLoc
+      );
     }
 
     @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
-    }
-
-    View.OnClickListener btnClickListener = new View.OnClickListener() {
-
-        @Override
-        public void onClick(View v) {
-
-            // TODO Auto-generated method stub
-            DownloadThread thread;
-
-            //Determine whether we're doing a KB or MB download
-            int dlSize = _sizeSelect.getSelectedItem().toString().equals("KB") ?
-                        Integer.parseInt(_nIterations.getText().toString()) :
-                        Integer.parseInt(_nIterations.getText().toString()) * 1024;
-            /*
-                Total number of segments to download is total size/8
-                as traffic generator's max size is 8KB
-             */
-            dlSize = dlSize/8;
-            int timeOut = Integer.parseInt(_timeOut.getText().toString());
-
-            String downloadPrefix = _prefixes.get(_locationSelect.getSelectedItem().toString());
-
-            _proDlg = ProgressDialog.show(MainActivity.this, "", "waiting...");
-
-            //Allow the app to continue downloading even when the screen is turned off
-            _mgr = (PowerManager) getApplicationContext().getSystemService(Context.POWER_SERVICE);
-            wakelock = _mgr.newWakeLock(PowerManager.PARTIAL_WAKE_LOCK, "MyWakeLock");
-            wakelock.acquire();
-
-            thread = new DownloadThread(dlSize, timeOut, downloadPrefix);
-
-            thread.start();
-        }
-    };
-
-    private class PingTimer implements OnData, OnTimeout
-    {
-        private long startTime;
-        private long elapsedTime = 0;
-        public int callbackCount_ = 0;
-
-        public long getElapsedTime()
-        {
-            return elapsedTime;
-        }
-
-        public void onData(Interest interest, Data data)
-        {
-            ++callbackCount_;
-
-            Log.i(TAG, "Got data packet with name " + data.getName().toUri());
-            elapsedTime = System.currentTimeMillis() - this.startTime;
-
-            String name = data.getName().toUri();
-            String pingTarget = name.substring(0, name.lastIndexOf("/"));
-            String contentStr = pingTarget + ": " + String.valueOf(elapsedTime) + " ms";
-            Log.i(TAG, "Content " + contentStr);
-
-        }
-
-        public void onTimeout(Interest interest)
-        {
-            ++callbackCount_;
-            Log.i(TAG, "Time out for interest " + interest.getName().toUri());
-        }
-
-        public void startUp()
-        {
-            startTime = System.currentTimeMillis();
-        }
-    }
-
-    private class DownloadThread extends Thread {
-        private int _nIterations;
-        private int _timeOut;
-        private String _downloadPrefix;
-        private Statistics _statistics;
-
-        public DownloadThread(int iterations, int timeout, String dlPrefix)
-        {
-            _nIterations = (iterations > 0) ? iterations : 5;
-            _timeOut = (timeout > 0) ? timeout : 1000;
-            _downloadPrefix = dlPrefix;
-            _statistics = new Statistics(MainActivity.this.getApplicationContext());
-        }
-
-        @Override
+    public void run() {
+      runOnUiThread(new Runnable() {
         public void run() {
-            Face face = new Face();
-            _statistics.beginResourceMonitor();
-            long testStart = System.currentTimeMillis();
+          errorPanel_.setText("");
+        }
+      });
+      Face face = new Face();
+      statistics_.beginResourceMonitor();
+      long testStart = System.currentTimeMillis();
 
-            try {
+      try {
+        for (int i = 0; i < nIterations_; i++) {
+          PingTimer timer = new PingTimer(statistics_);
 
-                for(int ii = 0; ii < _nIterations; ii++) {
-
-                    PingTimer timer = new PingTimer();
-
-                    Name name = new Name(_downloadPrefix + "8kB/" + String.valueOf(ii));
-                    Interest interest = new Interest(name, _timeOut);
-                    interest.setMustBeFresh(true);
-                    timer.startUp();
-                    face.expressInterest(interest, timer, timer);
-
-                    while(timer.callbackCount_ < 1){
-                        face.processEvents();
-                        Thread.sleep(5);
-                    }
-
-                    if(timer.getElapsedTime() != 0) {
-                        _statistics.addItem(timer.getElapsedTime());
-                    }
-
-                    else{   //make note that it's timed out, but otherwise continue
-                        _statistics.addTimeout();
-                    }
-                }
-
-                long testEnd = System.currentTimeMillis() - testStart;
-                _statistics.stopResourceMonitor();
-
-                File logfile = new File(getExternalFilesDir(null), "results.log");
-                BufferedWriter buf = null;
-
-                if(!logfile.exists()){
-                    logfile.createNewFile();
-                    buf = new BufferedWriter(new FileWriter(logfile, true));
-                    buf.append("Total Time (s)\tSize (MB)\tNum Timeouts\tAverage time\tStd Dev\tMin Time\tMax Time\tBattery % Usage");
-                    buf.newLine();
-                    buf.close();
-                }
-
-                buf = new BufferedWriter(new FileWriter(logfile, true));
-                buf.append(String.valueOf(testEnd / 1000) + "\t" + _statistics.dumpStats());
-                buf.newLine();
-                buf.close();
-            }
-
-            catch(Exception e){
-                Log.i(TAG, "Error:" + e.getMessage());
-            }
-
-            wakelock.release();
-            _proDlg.dismiss();
+          Name name = new Name(downloadPrefix_ + "8kB/" +
+                  String.valueOf(i));
+          Interest interest = new Interest(name, timeOut_);
+          interest.setMustBeFresh(true);
+          timer.startUp();
+          face.expressInterest(interest, timer, timer);
+          Thread.sleep(10);
+          //Process events every 20 Interests to ensure nothing times out.
+          if (i % 20 == 0) {
+            face.processEvents();
+            Thread.sleep(10);
+          }
         }
 
+        while ((statistics_.getSuccess() + statistics_.getTimeouts()) <
+            nIterations_) {
+        face.processEvents();
+        Thread.sleep(10);
+        }
+
+        long testEnd = System.currentTimeMillis() - testStart;
+        statistics_.setTotalTime(testEnd);
+        statistics_.stopResourceMonitor();
+        while (statistics_.isCpuMonitorAlive()) {
+          //no-op to ensure the top dump file is closed
+        }
+        if (statistics_.getSuccess() != 0) {
+          statistics_.writeLogFile();
+          runOnUiThread(
+              new Runnable() {
+                public void run() {
+                  errorPanel_.setText(SUCCESS_MSG);
+                }
+              }
+          );
+        }
+        else {
+          runOnUiThread(
+              new Runnable() {
+                public void run() {
+                  errorPanel_.setText(TIMEOUT_MSG);
+            }
+          });
+        }
+      }
+      catch (Exception e) {
+        Log.i(TAG, "Error:" + e.getMessage());
+      }
+
+      wakelock_.release();
+      proDlg_.dismiss();
     }
+
+  }
 }
--- app/src/main/java/com/ndn/bmmurphy/ndndl/Statistics.java
+++ app/src/main/java/com/ndn/bmmurphy/ndndl/Statistics.java
@@ -4,10 +4,13 @@ import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
 import android.os.BatteryManager;
+import android.os.Environment;
+import android.util.Log;
 
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.File;
+import java.io.FileInputStream;
 import java.io.FileWriter;
 import java.io.InputStreamReader;
 import java.math.RoundingMode;
@@ -18,147 +21,249 @@ import java.util.ArrayList;
  * Created by Ben Murphy on 6/10/15.
  */
 public class Statistics {
-    private static final String TAG = "NDNdl";
-    private ArrayList<Long> responseTimes_;
-    private int nTimeouts_;
-    private long minTime_;
-    private long maxTime_;
-    private double average_ = 0, variance_ = 0, stdDev_ = 0;
-    private double batteryStart_, batteryUsed_;
-    private Context context_;
-    private CpuMonitorThread cpu_;
-    private String topTest_;
+	
+  private static final String TAG = "NDNdl";
+  private ArrayList<Long> responseTimes_;
+  private int nTimeouts_;
+  private int timeoutMs_;
+  private long totalTime_;
+  private long minTime_;
+  private long maxTime_;
+  private double average_ = 0, variance_ = 0, stdDev_ = 0;
+  private double batteryStart_, batteryUsed_;
+  private Context context_;
+  private CpuMonitorThread cpu_;
+  private String topTest_;
+  private String location_;
+  private double cpuUsage_ = 0;
 
-    public Statistics(Context context){
-        minTime_ = Long.MAX_VALUE;
-        maxTime_ = Long.MIN_VALUE;
-        nTimeouts_ = 0;
-        responseTimes_ = new ArrayList<>();
-        context_ = context;
+  public Statistics(Context context, int timeout, String loc) {
+    minTime_ = Long.MAX_VALUE;
+    maxTime_ = Long.MIN_VALUE;
+    nTimeouts_ = 0;
+    responseTimes_ = new ArrayList<>();
+    context_ = context;
+    timeoutMs_ = timeout;
+    location_ = loc;
+  }
+
+  public void setTotalTime(long time) {
+    totalTime_ = time;
+  }
+  public int getSuccess() {
+    return responseTimes_.size();
+  }
+  public int getTimeouts() {
+    return nTimeouts_;
+  }
+
+  public boolean isCpuMonitorAlive() {
+    return cpu_.isAlive();
+  }
+
+  public void addItem(Long ii) {
+    responseTimes_.add(ii);
+    if (ii < minTime_) {
+      minTime_ = ii;
     }
+    if (ii > maxTime_) {
+      maxTime_ = ii;
+    }
+  }
 
-    public void addItem(Long ii){
-        responseTimes_.add(ii);
-        if(ii < minTime_){
-            minTime_ = ii;
+  public void addTimeout() {
+    nTimeouts_++;
+  }
+
+  public void calculateStatistics() {
+    calcVariance();
+    calcMean();
+  }
+
+  public void beginResourceMonitor() {
+    cpu_ = new CpuMonitorThread();
+    cpu_.start();
+    startBatteryMonitor(context_);
+  }
+
+  public void stopResourceMonitor() {
+    cpu_.interrupt();
+    stopBatteryMonitor(context_);
+  }
+
+  public void writeLogFile() {
+    File logfile = new File(Environment.getExternalStoragePublicDirectory(
+        Environment.DIRECTORY_DOCUMENTS), "NDNdl_results.log");
+    Environment.getExternalStoragePublicDirectory(
+        Environment.DIRECTORY_DOCUMENTS).mkdirs();
+    BufferedWriter buf = null;
+    try {
+      if (!logfile.exists()) {
+        logfile.createNewFile();
+        buf = new BufferedWriter(new FileWriter(logfile, true));
+        String header = "Location\t" +
+            "Total Time (s)\t" +
+            "Interest Timeout (ms)\t" +
+            "Size (MB)\t" +
+            "Throughput (MB/s)\t" +
+            "Seg Timeouts\t" +
+            "Timeout Percentage\t" +
+            "Average Seg Time (ms)\t" +
+            "Std Dev\t" +
+            "Min Time\t" +
+            "Max Time\t" +
+            "Battery % Usage\t" +
+            "Avg NFD CPU % Usage";
+        buf.append(header);
+        buf.newLine();
+        buf.close();
+      }
+
+      buf = new BufferedWriter(new FileWriter(logfile, true));
+      buf.append(dumpStats());
+      buf.newLine();
+      buf.close();
+    }
+    catch (Exception e) {
+      Log.i(TAG, "Error: " + e.getMessage());
+    }
+  }
+  private String dumpStats() {
+    calculateStatistics();
+    calculateCpuUsage();
+    DecimalFormat df = new DecimalFormat("#.##");
+    df.setRoundingMode(RoundingMode.HALF_UP);
+
+    double dlSize = (responseTimes_.size() + nTimeouts_) / 128.0;
+
+    String toReturn = location_ + "\t" +
+        df.format(totalTime_ / 1000) + "\t" +
+        String.valueOf(timeoutMs_) + "\t" +
+        df.format(dlSize) + "\t" +
+        df.format(dlSize / (totalTime_/1000)) + "\t" +
+        df.format(nTimeouts_) + "\t" +
+        df.format(100 * (double)nTimeouts_ / (
+            nTimeouts_ + responseTimes_.size())) + "\t" +
+        df.format(average_) + "\t" +
+        df.format(stdDev_) + "\t" +
+        df.format(minTime_) + "\t" +
+        df.format(maxTime_) + "\t" +
+        df.format(batteryUsed_) + "\t" +
+        df.format(cpuUsage_);
+
+    return toReturn;
+  }
+
+  private void startBatteryMonitor(Context context) {
+    IntentFilter iFilter = new IntentFilter(Intent.ACTION_BATTERY_CHANGED);
+    Intent batteryStatus = context.registerReceiver(null, iFilter);
+    int bStart = batteryStatus.getIntExtra(BatteryManager.EXTRA_LEVEL, -1);
+    batteryStart_ = bStart;
+  }
+
+  private void stopBatteryMonitor(Context context) {
+    IntentFilter iFilter = new IntentFilter(Intent.ACTION_BATTERY_CHANGED);
+    Intent batteryStatus = context.registerReceiver(null, iFilter);
+    int batteryStop = batteryStatus.getIntExtra(
+        BatteryManager.EXTRA_LEVEL, -1);
+    batteryUsed_ = batteryStart_ - batteryStop;
+
+  }
+
+  private class CpuMonitorThread extends Thread {
+    public void run() {
+      BufferedReader buf;
+      File logfile;
+      BufferedWriter bufferedWriter;
+      try {
+        logfile = new File(context_.getExternalFilesDir(null),
+            "topdump.log");
+        if (!logfile.exists()) {
+          logfile.createNewFile();
         }
-        if(ii > maxTime_){
-            maxTime_ = ii;
+        bufferedWriter = new BufferedWriter(new FileWriter(logfile,
+            false));
+      }
+      catch (Exception e) {
+        Log.i(TAG, "Error: " + e.getMessage());
+      }
+      Process process = null;
+      while (true) {
+        try {
+          process = Runtime.getRuntime().exec(
+              "/system/bin/top -m 7 -n 1");
+          String tmp;
+          buf = new BufferedReader(new InputStreamReader(
+              process.getInputStream()));
+          StringBuilder log = new StringBuilder();
+          while ((tmp = buf.readLine())!= null) {
+            tmp += " ";
+            log.append(tmp);
+            log.append("\n");
+          }
+          buf.close();
+          process.destroy();
+          topTest_ = log.toString();
+
+          bufferedWriter.append(topTest_);
+          //bufferedWriter.newLine();
+          Thread.sleep(430);
+        } catch (Exception e) {
+          try {
+            bufferedWriter.close();
+            process.destroy();
+          }
+          catch (Exception f){ }
+          break;
         }
+      }
     }
+  }
 
-    public void calculateStatistics(){
-        calcVariance();
-        calcMean();
-    }
-
-    public void addTimeout(){
-        nTimeouts_++;
-    }
-
-    public void beginResourceMonitor(){
-        cpu_ = new CpuMonitorThread();
-        cpu_.start();
-        startBatteryMonitor(context_);
-    }
-
-    public void stopResourceMonitor(){
-        cpu_.interrupt();
-        stopBatteryMonitor(context_);
-    }
-
-    private void startBatteryMonitor(Context context){
-        IntentFilter iFilter = new IntentFilter(Intent.ACTION_BATTERY_CHANGED);
-        Intent batteryStatus = context.registerReceiver(null, iFilter);
-        int bStart = batteryStatus.getIntExtra(BatteryManager.EXTRA_LEVEL, -1);
-        batteryStart_ = bStart;
-    }
-
-    private void stopBatteryMonitor(Context context){
-        IntentFilter iFilter = new IntentFilter(Intent.ACTION_BATTERY_CHANGED);
-        Intent batteryStatus = context.registerReceiver(null, iFilter);
-        int batteryStop = batteryStatus.getIntExtra(BatteryManager.EXTRA_LEVEL, -1);
-        batteryUsed_ = batteryStart_ - batteryStop;
-
-    }
-
-    private class CpuMonitorThread extends Thread{
-        public void run(){
-            BufferedReader buf;
-            File logfile;
-            BufferedWriter bufferedWriter;
-            try{
-                logfile = new File(context_.getExternalFilesDir(null), "topdump.log");
-                if(!logfile.exists()){
-                    logfile.createNewFile();
-                }
-                bufferedWriter = new BufferedWriter(new FileWriter(logfile, true));
-            }
-            catch(Exception e){
-                return;
-            }
-            Process process = null;
-            while(true) {
-                try {
-                    process = Runtime.getRuntime().exec("/system/bin/top -m 7 -n 1");
-                    String tmp;
-                    buf = new BufferedReader(new InputStreamReader(process.getInputStream()));
-                    StringBuilder log = new StringBuilder();
-                    while ((tmp = buf.readLine())!= null) {
-                        log.append(tmp);
-                        log.append("\n");
-                    }
-                    buf.close();
-                    process.destroy();
-                    topTest_ = log.toString();
-
-                    bufferedWriter.append(topTest_);
-                    bufferedWriter.newLine();
-                    Thread.sleep(430);
-                } catch (Exception e) {
-                    try {
-                        bufferedWriter.close();
-                        process.destroy();
-                    }
-                    catch(Exception f){ }
-                    break;
-                }
-            }
+  private void calculateCpuUsage() {
+    try{
+      BufferedReader br = null;
+      br = new BufferedReader(new InputStreamReader(new FileInputStream(
+          new File(context_.getExternalFilesDir(null),
+              "topdump.log"))));
+      File file = new File(context_.getExternalFilesDir(null),
+          "topdump.log");
+      int nSamples = 0;
+      int cpuTotal = 0;
+      String line;
+      while ((line = br.readLine()) != null) {
+        if (line.matches("(.*)PID(.*)")) {
+          nSamples++;
         }
-    }
-
-    public String dumpStats(){
-        calculateStatistics();
-        DecimalFormat df = new DecimalFormat("#.##");
-        df.setRoundingMode(RoundingMode.HALF_UP);
-        return df.format(responseTimes_.size()/128.0) + "\t" +
-                df.format(nTimeouts_) + "\t" +
-                df.format(average_) + "\t" +
-                df.format(stdDev_) + "\t" +
-                df.format(minTime_) + "\t" +
-                df.format(maxTime_) + "\t" +
-                df.format(batteryUsed_);
-    }
-
-    public String toString(){
-        return "";
-    }
-
-    private void calcVariance(){
-        double var = 0;
-        calcMean();
-        for(long ll: responseTimes_){
-            var += Math.pow((average_ - ll), 2);
+        if (line.matches("(.*)NfdService(.*)")) {;
+          String[] tokens = line.split("\\s+");
+          cpuTotal += Integer.parseInt(tokens[2].substring(
+              0, tokens[2].length() - 1));
+          cpuUsage_ = cpuTotal / (double)nSamples;
         }
-        variance_ = var/ responseTimes_.size();
-        stdDev_ = Math.sqrt(variance_);
-    }
 
-    private void calcMean(){
-        long total = 0;
-        for(long ll: responseTimes_){
-            total += ll;
-        }
-        average_ = total / (double) responseTimes_.size();
+      }
     }
+    catch (Exception e) {
+      Log.i(TAG, "Error: " + e.getMessage());
+    }
+  }
+
+  private void calcVariance() {
+    double var = 0;
+    calcMean();
+    for (long ll: responseTimes_) {
+      var += Math.pow((average_ - ll), 2);
+    }
+    variance_ = var / responseTimes_.size();
+    stdDev_ = Math.sqrt(variance_);
+  }
+
+  private void calcMean() {
+    long total = 0;
+    for (long ll: responseTimes_) {
+      total += ll;
+    }
+    average_ = total / (double) responseTimes_.size();
+  }
 }
